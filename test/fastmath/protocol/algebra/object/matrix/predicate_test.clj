fcf26bea4f5a980027b91d938ecb611e614f6f0d
ctw <clark.thomaswilliam@zoho.eu>
2026-02-02 20:23:11 +0100

    tidying

---
+++ test/fastmath/protocol/algebra/object/matrix/predicate_test.clj
@@ -0,0 +1,10 @@
+(ns fastmath.protocol.algebra.object.matrix.predicate-test
+  (:require [fastmath.protocol.algebra.object.matrix.extra :as sut]
+            [clojure.test :as t :refer [deftest are]]
+            [fastmath.algebra.object.number.complex.create :as C]
+            [fastmath.algebra.object.matrix.create :as matrix]))
+
+(deftest square
+  (are [e a] (= e a)
+
+    (sut/square)))
